a59741ef255eb89bc040442958ccbcccb4beb1d2
kevinreber <kevinreber1@gmail.com>
2020-05-03 15:54:11 -0700

    test user model

--- test_user_model.py
+++ test_user_model.py
@@ -5,10 +5,11 @@
 #    python -m unittest test_user_model.py
 
 
+from app import app
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follows
+from models import db, User, Message, Follows, Likes
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -20,7 +21,6 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
 # Now we can import app
 
-from app import app
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
@@ -35,24 +35,74 @@ class UserModelTestCase(TestCase):
     def setUp(self):
         """Create test client, add sample data."""
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        db.drop_all()
+        db.create_all()
 
-        self.client = app.test_client()
+        self.uid = 12345
+        u = User.signup("test", "test@test.com", "password", None)
 
-    def test_user_model(self):
-        """Does basic model work?"""
-
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
-
+        u.id = self.uid
         db.session.add(u)
         db.session.commit()
 
+        self.u = User.query.get(self.uid)
+        self.client = app.test_client()
+
+    def tearDown(self):
+        res = super().tearDown()
+        db.session.rollback()
+        return res
+
+    def test_user_model(self):
+        """Does basic user model work?"""
+
         # User should have no messages & no followers
-        self.assertEqual(len(u.messages), 0)
-        self.assertEqual(len(u.followers), 0)
+        self.assertEqual(len(self.u.messages), 0)
+        self.assertEqual(len(self.u.followers), 0)
+
+    def test_message_model(self):
+        """Does basic message model work"""
+
+        m = Message(
+            text="Testing 123...",
+            user_id=self.uid
+        )
+
+        db.session.add(m)
+        db.session.commit()
+
+        # User should have one message
+        self.assertEqual(len(self.u.messages), 1)
+        self.assertEqual(self.u.messages[0].text, "Testing 123...")
+
+    def test_message_likes(self):
+        """Tests when new user likes another users messages"""
+
+        m1 = Message(
+            text="Test 1",
+            user_id=self.uid
+        )
+
+        m2 = Message(
+            text="Test 2",
+            user_id=self.uid
+        )
+
+        new_user = User.signup(
+            "newtest", "new@test.com", "password", None)
+
+        new_uid = 67890
+        new_user.id = new_uid
+
+        db.session.add_all([m1, m2, new_user])
+        db.session.commit()
+
+        # append message to new_user.likes
+        new_user.likes.append(m1)
+        db.session.commit()
+
+        # Get all likes of new_user
+        likes = Likes.query.filter(new_user.id == Likes.user_id).all()
+
+        self.assertEqual(len(likes), 1)
+        self.assertEqual(likes[0].message_id, m1.id)
