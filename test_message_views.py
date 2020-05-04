ddfaa1b3aad1b44bbe951587050929caae125739
kevinreber <kevinreber1@gmail.com>
2020-05-03 17:46:56 -0700

    tested: user index, user search, user show

--- test_message_views.py
+++ test_message_views.py
@@ -5,10 +5,11 @@
 #    FLASK_ENV=production python -m unittest test_message_views.py
 
 
+from app import app, CURR_USER_KEY
 import os
 from unittest import TestCase
 
-from models import db, connect_db, Message, User
+from models import db, connect_db, Message, User, Likes, Follows
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -20,7 +21,6 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
 # Now we can import app
 
-from app import app, CURR_USER_KEY
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
@@ -49,7 +49,54 @@ class MessageViewTestCase(TestCase):
                                     password="testuser",
                                     image_url=None)
 
+        self.testuser_id = 9999
+        self.testuser.id = self.testuser_id
+
+        self.u1 = User.signup("abc", "test1@test.com", "password", None)
+        self.u1_id = 1111
+        self.u1.id = self.u1_id
+        self.u2 = User.signup("efg", "test2@test.com", "password", None)
+        self.u2_id = 2222
+        self.u2.id = self.u2_id
+        self.u3 = User.signup("hij", "test3@test.com", "password", None)
+        self.u4 = User.signup("testing", "test4@test.com", "password", None)
+
         db.session.commit()
+
+    def tearDown(self):
+        resp = super().tearDown()
+        db.session.rollback()
+        return resp
+
+    def test_user_index(self):
+        with self.client as c:
+            resp = c.get("/users")
+
+            # all usernames should appear
+            self.assertIn("@testuser", str(resp.data))
+            self.assertIn("@abc", str(resp.data))
+            self.assertIn("@efg", str(resp.data))
+            self.assertIn("@hij", str(resp.data))
+            self.assertIn("@testing", str(resp.data))
+
+    def test_users_search(self):
+        with self.client as c:
+            resp = c.get("/users?q=test")
+
+            # should only show users with "test" in username
+            self.assertIn("@testuser", str(resp.data))
+            self.assertIn("@testing", str(resp.data))
+
+            self.assertNotIn("@abc", str(resp.data))
+            self.assertNotIn("@efg", str(resp.data))
+            self.assertNotIn("@hij", str(resp.data))
+
+    def test_user_show(self):
+        with self.client as c:
+            resp = c.get(f"users/{self.testuser.id}")
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("@testuser", str(resp.data))
 
     def test_add_message(self):
         """Can use add a message?"""
